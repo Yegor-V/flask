91854aa3591d47a6e67eddf8aba4bc95756c3d45
yv <vasilchenko.yegor@gmail.com>
2017-03-09 22:14:40 +0200

    ready api with tons of copypaste

--- evo.py
+++ evo.py
@@ -6,6 +6,7 @@ import time
 from sqlalchemy.exc import DataError, IntegrityError
 
 from credentials import POSTGRES_CREDENTIALS, SECRET_KEY
+from utils import to_timestamp
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
@@ -29,6 +30,14 @@ def get_all_positions():
 
 def get_all_departments():
     return [get_object_dict(d) for d in Department.query.all()]
+
+
+def get_department_vacancies(department_id):
+    return [get_object_dict(v) for v in Vacancy.query.filter_by(department_id=department_id)]
+
+
+def get_department_employees(department_id):
+    return [get_object_dict(e) for e in Employee.query.filter_by(department_id=department_id)]
 
 
 class Department(db.Model):
@@ -81,20 +90,26 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(DATABASE_STRING_LENGTH), nullable=False)
     surname = db.Column(db.String(DATABASE_STRING_LENGTH), nullable=False)
+    position_id = db.Column(db.Integer, db.ForeignKey('position.id'))
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id'))
     email = db.Column(db.String(DATABASE_STRING_LENGTH))
     phone = db.Column(db.String(DATABASE_STRING_LENGTH))
     birth_date = db.Column(db.String(DATABASE_STRING_LENGTH))
     start_work_date = db.Column(db.BigInteger)
-    position_id = db.Column(db.Integer, db.ForeignKey('position.id'))
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'))
     is_department_leader = db.Column(db.Boolean, default=False)
 
-    def __init__(self, name, surname, position_id, department_id):
+    def __init__(self, name, surname, position_id, department_id,
+                 email=None, phone=None, birth_date=None,
+                 start_work_date=int(time.time()), is_department_leader=False):
         self.name = name
         self.surname = surname
         self.position_id = position_id
         self.department_id = department_id
-        self.start_work_date = int(time.time())
+        self.email = email
+        self.phone = phone
+        self.birth_date = birth_date
+        self.start_work_date = start_work_date
+        self.is_department_leader = is_department_leader
 
     def __repr__(self):
         return 'Employee {} {}'.format(self.name, self.surname)
@@ -189,7 +204,7 @@ class DepartmentApi(Resource):
         Deletes department. department_id is needed.
         :return: success/error json
         """
-        department_id = request.form.get('department_id')
+        department_id = request.form.get('id')
         if not department_id:
             return {'error': 'department_id is required'}, 400
         else:
@@ -290,9 +305,20 @@ class VacancyApi(Resource):
     @staticmethod
     def get():
         """
-        :return: All vacancies json of current department. department name is needed in request.
+        :return: All vacancies json of current department. department id is needed in request.
         """
-        raise NotImplementedError
+        department_id = request.args.get('department_id')
+        if not department_id:
+            return {'error': 'department_id id required'}, 400
+        else:
+            try:
+                department = Department.query.filter_by(id=department_id).first()
+                if not department:
+                    return {'error': 'department with specified id not found'}, 404
+                else:
+                    return get_department_vacancies(department_id)
+            except DataError:
+                return {'error': 'department_id must be int'}, 400
 
     @staticmethod
     def post():
@@ -301,7 +327,40 @@ class VacancyApi(Resource):
         date_opened is optional (defaults to now)
         :return: success/error json
         """
-        raise NotImplementedError
+        department_id = request.form.get('department_id')
+        position_id = request.form.get('position_id')
+        date_opened = request.form.get('date_opened')
+
+        if not position_id or not department_id:
+            return {'error': 'position_id and department_id required'}, 400
+        else:
+            try:
+                department = Department.query.filter_by(id=department_id).first()
+                position = Position.query.filter_by(id=position_id).first()
+            except DataError:
+                return {'error': 'department_id and position_id must be ints'}, 400
+
+            if not department or not position:
+                return {'error': 'department or position not found'}, 404
+
+            else:
+                if date_opened:
+                    try:
+                        date_opened = to_timestamp(date_opened)
+                        vacancy = Vacancy(department_id=department_id, position_id=position_id, date_opened=date_opened)
+                    except ValueError:
+                        return {'error': 'date_opened must be in format mm/dd/yyyy'}, 400
+                else:
+                    vacancy = Vacancy(department_id=department_id, position_id=position_id)
+                db.session.add(vacancy)
+                db.session.commit()
+                return {
+                    'success': 'vacancy created',
+                    'vacancy': {
+                        'id': vacancy.id, 'department_id': vacancy.department_id, 'date_opened': vacancy.date_opened,
+                        'position': {'id': vacancy.position.id, 'name': vacancy.position.name}
+                    }
+                }
 
     @staticmethod
     def patch():
@@ -309,7 +368,42 @@ class VacancyApi(Resource):
         Updates vacancy data.
         :return: success/error json
         """
-        raise NotImplementedError
+        vacancy_id = request.form.get('vacancy_id')
+        new_department_id = request.form.get('new_department_id')
+        new_position_id = request.form.get('new_position_id')
+        new_date_opened = request.form.get('new_date_opened')
+
+        if not vacancy_id or not all((new_department_id, new_position_id, new_date_opened)):
+            return {
+                'error': 'vacancy_id and at least one of new_department_id, new_position_id, new_date_opened needed'
+            }, 400
+        else:
+            try:
+                vacancy = Vacancy.query.filter_by(id=vacancy_id).first()
+                if not vacancy:
+                    return {'error': 'vacancy not found'}, 404
+                if new_department_id:
+                    vacancy.department_id = new_department_id
+                if new_position_id:
+                    vacancy.position_id = new_position_id
+                if new_date_opened:
+                    try:
+                        vacancy.date_opened = to_timestamp(new_date_opened)
+                    except ValueError:
+                        return {'error': 'date_opened must be in format mm/dd/yyyy'}, 400
+                db.session.add(vacancy)
+                db.session.commit()
+                return {
+                    'success': 'vacancy updated',
+                    'vacancy': {
+                        'id': vacancy.id,
+                        'date_opened': vacancy.date_opened,
+                        'department': {'id': vacancy.department_id, 'name': vacancy.department.name},
+                        'position': {'id': vacancy.position_id, 'name': vacancy.position.name}
+                    }
+                }
+            except (DataError, IntegrityError):
+                return {'error': 'bad department_id, position_id, vacancy_id'}
 
     @staticmethod
     def delete():
@@ -317,16 +411,53 @@ class VacancyApi(Resource):
         Deletes (closes) vacancy.
         :return: success/error json
         """
-        raise NotImplementedError
+        vacancy_id = request.form.get('vacancy_id')
+        if not vacancy_id:
+            return {'error': 'vacancy_id is required'}, 400
+        else:
+            try:
+                vacancy = Vacancy.query.filter_by(id=vacancy_id).first()
+                if not vacancy:
+                    return {'error': 'vacancy with specified id not found'}, 404
+                else:
+                    db.session.delete(vacancy)
+                    db.session.commit()
+                    return {'success': 'vacancy deleted'}
+            except DataError:
+                return {'error': 'vacancy_id must be string'}, 400
 
 
 class EmployeeApi(Resource):
     @staticmethod
     def get():
         """
-        :return: All employees of current department json. Needs department_name parameter.
+        :return: All employees of current department json. Needs department_id parameter.
         """
-        raise NotImplementedError
+        department_id = request.args.get('department_id')
+        employee_id = request.args.get('employee_id')
+
+        if not department_id and not employee_id:
+            return {'error': 'department_id or/and employee_id is required'}, 400
+
+        if employee_id:
+            try:
+                employee = Employee.query.filter_by(id=employee_id).first()
+                if not employee:
+                    return {'error': 'employee with specified employee_id not found'}, 404
+                else:
+                    return get_object_dict(employee)
+            except DataError:
+                return {'error': 'employee_id must be a string'}, 400
+
+        elif department_id:
+            try:
+                department = Department.query.filter_by(id=department_id).first()
+                if not department:
+                    return {'error': 'department with specified id not found'}, 404
+                else:
+                    return get_department_employees(department_id)
+            except DataError:
+                return {'error': 'department_id must be a string'}, 400
 
     @staticmethod
     def post():
@@ -337,7 +468,27 @@ class EmployeeApi(Resource):
         If is_department_leader is not set -> will be set to False.
         :return: success/error json
         """
-        raise NotImplementedError
+        position_id = request.form.get('position_id')
+        department_id = request.form.get('department_id')
+        name = request.form.get('name')
+        surname = request.form.get('surname')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        birth_date = request.form.get('birth_date')
+        start_work_date = request.form.get('start_work_date')
+        is_department_leader = request.form.get('is_department_leader')
+
+        if not all((position_id, department_id, name, surname)):
+            return {'error': 'position_id, department_id, name, surname required'}, 400
+        else:
+            try:
+                employee = Employee(position_id=position_id, department_id=department_id, name=name, surname=surname,
+                                    email=email, phone=phone, birth_date=birth_date, start_work_date=start_work_date,
+                                    is_department_leader=is_department_leader)
+                db.session.add(employee)
+                db.session.commit()
+            except Exception as e:
+                return {'error': 'failed to create employee: {}'.format(e)}
 
     @staticmethod
     def patch():
@@ -354,7 +505,20 @@ class EmployeeApi(Resource):
         Deletes employee. Needs employee_id in request. ("FIRE" button)
         :return: success/error json
         """
-        raise NotImplementedError
+        employee_id = request.form.get('employee_id')
+        if not employee_id:
+            return {'error': 'employee_id is required'}, 400
+        else:
+            try:
+                employee = Employee.query.filter_by(id=employee_id).first()
+                if not employee:
+                    return {'error': 'employee with specified id not found'}, 404
+                else:
+                    db.session.delete(employee)
+                    db.session.commit()
+                    return {'success': 'employee deleted'}
+            except DataError:
+                return {'error': 'employee_id must be string'}, 400
 
 
 api.add_resource(DepartmentApi, '/api/department/')
--- utils.py
+++ utils.py
@@ -4,3 +4,8 @@ from datetime import datetime, timezone
 def to_timestamp(date_string):
     dt = datetime.strptime(date_string, '%m/%d/%Y')
     return int(dt.replace(tzinfo=timezone.utc).timestamp())
+
+
+def from_timestamp(timestamp):
+    timestamp = int(timestamp)
+    return datetime.utcfromtimestamp(timestamp).strftime('%m/%d/%Y')
