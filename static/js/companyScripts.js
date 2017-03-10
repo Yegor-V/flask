15099eafec3afebfff380460dbba8017070fa38f
yv <vasilchenko.yegor@gmail.com>
2017-03-11 01:02:51 +0200

    100 lines js copy-paste working crunch

--- static/js/companyScripts.js
+++ static/js/companyScripts.js
@@ -42,10 +42,18 @@ function getAllPositions() {
 /**Create div with department/position*/
 function createSimpleHtmlDiv(dict, type) {
     var div = document.createElement('div');
+    var nameDiv = document.createElement('span');
+    var descriptionDiv = document.createElement('span');
     div.id = type + '-' + dict['id'];
-    div.innerHTML = dict['name'] + ' ' + dict['description'];
-    var button = createDeleteButton(dict, type);
-    div.appendChild(button);
+    nameDiv.innerHTML = dict['name'] + ' ';
+    nameDiv.id = type + '-' + dict['id'] + '-name';
+    descriptionDiv.innerHTML = dict['description'];
+    descriptionDiv.id = type + '-' + dict['id'] + '-description';
+    div.appendChild(nameDiv);
+    div.appendChild(descriptionDiv);
+    var deleteButton = createDeleteButton(dict, type);
+    div.appendChild(deleteButton);
+    div.appendChild(createEditButton(dict, type));
     return div
 }
 
@@ -58,23 +66,105 @@ function createDeleteButton(dict, type) {
     return button
 }
 
+/**Creates 'Delete' button (used by createSimpleHtmlDiv*/
+function createEditButton(dict, type) {
+    var button = document.createElement('button');
+    button.id = type + '-' + dict['id'] + '-edit-button';
+    button.className = 'edit-' + type + '-button';
+    button.innerHTML = 'Edit';
+    return button
+}
+
 /**Catch click event on departments div and if delete button is hit - delete corresponding item*/
 var departmentsDiv = document.getElementById('departments');
 departmentsDiv.onclick = function (event) {
     var clickedOnId = event['path'][0].id;
     var clickedOnClass = event['path'][0].className;
+    var department_id = clickedOnId.split('-')[1];
 
     if (clickedOnClass === 'delete-department-button'){
-        var department_id = clickedOnId.split('-')[1];
         deleteItem(department_id, 'department')
     }
-    if (clickedOnClass === 'delete-position-button'){
-        var position_id = clickedOnId.split('-')[1];
-        deleteItem(position_id, 'position')
+    if (clickedOnClass === 'edit-department-button'){
+        editDepartment(department_id, event)
     }
-
+    if (clickedOnClass === 'send-edit-department'){
+        sendEditDepartment(event)
+    }
 };
 
+
+function editDepartment(department_id, event) {
+    var old_name = document.getElementById('department-' + department_id + '-name').textContent;
+    var old_description = document.getElementById('department-' + department_id + '-description').textContent;
+    var divToEdit = document.getElementById('department-' + department_id);
+
+    divToEdit.innerHTML = '';
+
+    var new_name_input_label = document.createElement('label');
+    new_name_input_label.for = 'department-new-name';
+    new_name_input_label.textContent = 'New name: ';
+    var new_name_input = document.createElement('input');
+    new_name_input.id = 'department-' + department_id + '-new-name';
+    new_name_input.name= 'new_name';
+    new_name_input.type = 'text';
+    new_name_input.value = old_name;
+    var new_description_input_label = document.createElement('label');
+    new_description_input_label.for = 'department-new-description';
+    new_description_input_label.textContent = 'New description:';
+    var new_description_input = document.createElement('input');
+    new_description_input.id = 'department-' + department_id + '-new-description';
+    new_description_input.name= 'new_description';
+    new_description_input.type = 'text';
+    new_description_input.value = old_description;
+
+    var sendFormButton= document.createElement('button');
+    sendFormButton.textContent = 'Edit';
+    sendFormButton.className = 'send-edit-department';
+    sendFormButton.id = 'send-department-' + department_id + '-edit-form';
+
+    divToEdit.appendChild(new_name_input_label);
+    divToEdit.appendChild(new_name_input);
+    divToEdit.appendChild(new_description_input_label);
+    divToEdit.appendChild(new_description_input);
+    divToEdit.appendChild(sendFormButton);
+
+
+}
+
+function sendEditDepartment(event){
+    var department_id = event['path'][1].id.split('-')[1];
+    var new_name = document.getElementById('department-' + department_id + '-new-name').value;
+    var new_description = document.getElementById('department-' + department_id + '-new-description').value;
+
+    $.ajax('/api/department/',{
+        type: 'PATCH',
+        data: {
+            'department_id': department_id,
+            'new_name': new_name,
+            'new_description': new_description
+        },
+        success: function () {
+            var parentDiv = document.getElementById('department-' + department_id);
+            parentDiv.innerHTML = '';
+            var nameDiv = document.createElement('span');
+            var descriptionDiv = document.createElement('span');
+            nameDiv.innerHTML = new_name;
+            nameDiv.id = 'department-' + department_id + '-name';
+            descriptionDiv.innerHTML = new_description;
+            descriptionDiv.id = 'department-' + department_id + '-description';
+            parentDiv.appendChild(nameDiv);
+            parentDiv.appendChild(descriptionDiv);
+            var dict = {'id': department_id, 'name': new_name, 'description': new_description};
+            var deleteButton = createDeleteButton(dict, 'department');
+            parentDiv.appendChild(deleteButton);
+            parentDiv.appendChild(createEditButton(dict, 'department'));
+        },
+        error: function (data) {
+            console.log(data)
+        }
+    })
+}
 
 /**Send ajax delete-item request*/
 function deleteItem(id, type) {
